c76d22642564d86770f100f7e7106a58e181002e
javie <javier_mera88@hotmail.com>
2017-06-12 16:29:24 -0500

    Modified the search item layout wrapping all the views inside a cardview.

--- app/src/main/java/com/javier/learningbuddy/adapters/SearchAdapter.java
+++ app/src/main/java/com/javier/learningbuddy/adapters/SearchAdapter.java
@@ -41,7 +41,7 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchViewHolder> implem
         Item currentItem = this.items.get(position);
         holder.setTitle(currentItem.snippet.title);
         holder.setChannelTitle(currentItem.snippet.channelTitle);
-        holder.setThumbnail(currentItem.snippet.thumbnails.low.url);
+        holder.setThumbnail(currentItem.snippet.thumbnails.high.url);
     }
 
     @Override
